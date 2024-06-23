a2622c7ae9abffee1d2054fe2f358953e9a8a8e4
Olga Kuksa <89924674+wawaqa@users.noreply.github.com>
2024-06-24 01:31:47 +0300

    feat: add fetching sungle product from dynamodb

--- product_service/lambda_func/products_by_id.ts
+++ product_service/lambda_func/products_by_id.ts
@@ -1,11 +1,34 @@
-import { products } from "./products";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 export async function handler(event: APIGatewayProxyEvent) {
 
     const productId = event.pathParameters?.productId;
+    const productsTableName = process.env.PRODUCTS_TABLE_NAME || '';
+    const stocksTableName = process.env.STOCKS_TABLE_NAME || '';
+    const productsPrimaryKey = process.env.PRODUCTS_PRIMARY_KEY || '';
+    const stocksPrimaryKey = process.env.STOCKS_PRIMARY_KEY || '';
 
-    const product = products.find(p => p.id === productId);
+
+    if (!productsTableName || !stocksTableName) throw new Error("[ERROR: pls, make sure all the table names are passed to products list handler")
+    const db = DynamoDBDocument.from(new DynamoDB());
+    const productParams = {
+        TableName: productsTableName,
+        Key: {
+            [productsPrimaryKey]: productId
+        }
+    };
+    const { Item: product } = await db.get(productParams);
+
+    const stocksParams = {
+        TableName: stocksTableName,
+        Key: {
+            [stocksPrimaryKey]: productId
+        }
+    };
+    const { Item: stock } = await db.get(stocksParams);
+    const count = stock?.count || 0;
 
     if (!product) {
         return {
@@ -20,6 +43,6 @@ export async function handler(event: APIGatewayProxyEvent) {
     return {
         statusCode: 200,
         headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" },
-        body: JSON.stringify(product)
+        body: JSON.stringify({ ...product, count })
     };
 };
