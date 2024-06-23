d895ba34d34190e655082c7335e014ab2a9621c4
Olga Kuksa <89924674+wawaqa@users.noreply.github.com>
2024-06-24 01:12:43 +0300

    feat: add initial work with dynamodb

--- product_service/get_products.ts
+++ product_service/get_products.ts
@@ -2,17 +2,25 @@ import { Stack } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import path from 'path';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class GetProducts extends Construct {
     public getProductListFunction: lambda.Function;
     constructor(stack: Stack, constructId: string) {
         super(stack, constructId);
         const lambdaFuncDir = path.join(__dirname, '../product_service/lambda_func');
+        const productsTable = Table.fromTableName(this, 'ProductsTable', 'products');
+        const stocksTable = Table.fromTableName(this, 'StockTable', 'stocks');
         this.getProductListFunction = new lambda.Function(this, 'GetProductsListFunction', {
             runtime: lambda.Runtime.NODEJS_20_X,
             code: lambda.Code.fromAsset(lambdaFuncDir),
             handler: 'products_list.handler',
+            environment: {
+                PRODUCTS_TABLE_NAME: productsTable.tableName,
+                STOCKS_TABLE_NAME: stocksTable.tableName
+            },
         });
-
+        productsTable.grantReadData(this.getProductListFunction);
+        stocksTable.grantReadData(this.getProductListFunction)
     }
 }
--- product_service/lambda_func/products_list.ts
+++ product_service/lambda_func/products_list.ts
@@ -1,9 +1,31 @@
-import { products } from "./products";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 export async function handler() {
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" },
-        body: JSON.stringify(products)
-    };
+    const productsTableName = process.env.PRODUCTS_TABLE_NAME || '';
+    const stocksTableName = process.env.STOCKS_TABLE_NAME || '';
+    try {
+        if (!productsTableName || !stocksTableName) throw new Error("[ERROR: pls, make sure all the table names are passed to products list handler")
+        const db = DynamoDBDocument.from(new DynamoDB());
+        const { Items: products } = await db.scan({
+            TableName: productsTableName
+        });
+        const { Items: stocks } = await db.scan({
+            TableName: stocksTableName
+        });
+
+        const results = products?.map(product => ({ ...product, count: stocks?.find(it => it.product_id === product.id)?.count || 0 }))
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" },
+            body: JSON.stringify(results)
+        };
+    }
+    catch (e) {
+        return {
+            statusCode: 500,
+            headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" },
+            body: JSON.stringify(e)
+        }
+    }
 };
