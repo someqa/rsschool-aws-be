067a6374999a34ca22eaed709e4e29d88c229b91
Olga Kuksa <89924674+wawaqa@users.noreply.github.com>
2024-07-01 02:25:57 +0300

    feat: add import product file

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,24 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { ImportProductsFile } from './import_products_file';
+import { ImportProductsFile } from './import_products_file_stack';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const importProductsFile = new ImportProductsFile(this, 'ImportProductsFile').importProductsFileFn;
+    const importProductsFile = new ImportProductsFile(this, 'ImportProductsFileFn').importProductsFileFn;
 
     // Create the API Gateway
-    const api = new apigateway.RestApi(this, 'ImportProductsFile', {
-      restApiName: 'Product Service',
-      defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS
-      }
+    const api = new apigateway.RestApi(this, 'ImportProductsFileApi', {
+      restApiName: 'Import Products Service',
+      // defaultCorsPreflightOptions: {
+      //   allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      //   allowMethods: apigateway.Cors.ALL_METHODS
+      // }
     });
 
     const productsResource = api.root.addResource('import');
+    productsResource.addMethod('OPTIONS', new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            'method.response.header.Access-Control-Allow-Origin': "'*'",
+            'method.response.header.Access-Control-Allow-Methods': "'GET,POST,OPTIONS'",
+            'method.response.header.Access-Control-Max-Age': "'1728000'",
+          },
+          responseTemplates: {
+            'application/json': '{"statusCode": 200}'
+          },
+        },
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': '{"statusCode": 200}'
+      },
+    }), {
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': true,
+            'method.response.header.Access-Control-Allow-Methods': true,
+            'method.response.header.Access-Control-Allow-Origin': true,
+            'method.response.header.Access-Control-Max-Age': true,
+          },
+        },
+      ],
+    });
     productsResource.addMethod('GET', new apigateway.LambdaIntegration(importProductsFile));
   }
 }
---
+++ import-service/lib/import_products_file_stack.ts
@@ -0,0 +1,32 @@
+import { Stack } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import path from 'path';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class ImportProductsFile extends Construct {
+    public importProductsFileFn: lambda.Function;
+    constructor(stack: Stack, constructId: string) {
+        super(stack, constructId);
+        const bucket = s3.Bucket.fromBucketName(this, "Import-service-bucket", "someqa-import-service-bucket");
+        const lambdaFuncDir = path.join(__dirname, '../lambda_func');
+        this.importProductsFileFn = new lambda.Function(this, 'ImportProductsFileFunction', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            code: lambda.Code.fromAsset(lambdaFuncDir),
+            handler: 'import_products_file_lambda.handler',
+            environment: {
+                BUCKET_NAME: bucket.bucketName,
+            },
+        });
+        bucket.grantReadWrite(this.importProductsFileFn);
+
+        this.importProductsFileFn.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['s3:GetObject', 's3:PutObject', 's3:ListBucket'],
+            resources: [
+                bucket.bucketArn,
+                `${bucket.bucketArn}/*`
+            ],
+        }));
+    }
+}
